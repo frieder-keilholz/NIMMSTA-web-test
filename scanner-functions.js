770735f5b8897e6682fe039111a517307966a725
Frieder Keilholz <fgk1999@outlook.de>
2023-07-28 14:59:25 +0200

    device accesss to global

--- scanner-functions.js
+++ scanner-functions.js
@@ -2,11 +2,13 @@ NIMMSTA.websocketPort = 64693;
 NIMMSTA.requestSendTimeout = 1000; // 1000 ms until a request is considered 'failed'
 NIMMSTA.maxCommunicationRetries = 3; // Roughly 3 * 1000 ms = 3000 ms is the time until onError will be invoked if app is missing.
 
+var device;
+
 // Invoked once connection to app is ready to be used.
 NIMMSTA.onReady(function() {
     const connectionManager = new NimmstaConnectionManager();
     if (connectionManager.devices.length > 0) {
-        const device = connectionManager.devices[0];
+        device = connectionManager.devices[0];
         console.log(device)
         document.getElementById('address').innerHTML = device.address
         document.getElementById('bat-value').innerHTML = device.batteryLevel
